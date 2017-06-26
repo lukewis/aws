1b4bd69872845d12c6a9fb854b020eaab47ce9c9
lukewis <lukewis@users.noreply.github.com>
2017-06-26 05:39:51 -0400

    Moved the rest of the setup logic from the cloud formation template into the script

--- scripts/mongoanddockerinit.sh
+++ scripts/mongoanddockerinit.sh
@@ -27,4 +27,19 @@ if [[ $USERNAME = "" || $PASSWORD = "" ]]; then
 	exit
 fi
 
+# Create a filesystem on the mounted volume\n",
+sudo mkfs -t ext4 /dev/xvdh
+sudo mkdir -p /data/db
+sudo mount /dev/xvdh /data/db
+
+# Setup the repository for installing mongo
+echo "[mongodb-org-3.2]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.2/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc\n" > /etc/yum.repos.d/mongodb-org-3.2.repo
+sudo yum -y update
+# Install mongo server, shell and tools
+sudo yum install -y mongodb-org-server mongodb-org-shell mongodb-org-tools
+# Start the server and create the admin user
+sudo mongod --fork --logpath /var/log/mongod.log
 sudo mongo --eval "db = db.getSiblingDB('admin'); db.createUser({ user: '$USERNAME', pwd: '$PASSWORD',  roles: [ { role: 'userAdminAnyDatabase', db: 'admin' } ]})"
+# Restart the server with authentication
+sudo mongod --shutdown
+sudo mongod --fork --auth --logpath /var/log/mongod.log
