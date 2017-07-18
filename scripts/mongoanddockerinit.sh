a6c4507d4e5eaa7dfc4192c874a03a3daceebd60
lukewis <lukewis@users.noreply.github.com>
2017-07-18 05:16:47 -0400

    - Update cloudformation to use git instead of s3 - Update script to use cat instead of echo (had problems with line endings)

--- scripts/mongoanddockerinit.sh
+++ scripts/mongoanddockerinit.sh
@@ -34,19 +34,47 @@ sudo mkdir -p /data/db
 sudo mount /dev/xvdh /data/db
 
 # Setup the repository for installing mongo
-echo "[mongodb-org-3.2]
+cat << 'YOF' >> /etc/yum.repos.d/mongodb-org-3.2.repo
+[mongodb-org-3.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
-" > /etc/yum.repos.d/mongodb-org-3.2.repo
+YOF
+
 sudo yum -y update
 # Install mongo server, shell and tools
 sudo yum install -y mongodb-org-server mongodb-org-shell mongodb-org-tools
 # Start the server and create the admin user
 sudo mongod --fork --logpath /var/log/mongod.log
-sudo mongo --eval "db = db.getSiblingDB('admin'); db.createUser({ user: '$USERNAME', pwd: '$PASSWORD',  roles: [ { role: 'userAdminAnyDatabase', db: 'admin' } ]})"
+sudo mongo --eval "db = db.getSiblingDB('admin'); db.createUser({ user: '$USERNAME', pwd: '$PASSWORD',  roles: [ { role: 'root', db: 'admin' } ]})"
 # Restart the server with authentication
 sudo mongod --shutdown
 sudo mongod --fork --auth --logpath /var/log/mongod.log
+
+# Install dotnet core: https://www.microsoft.com/net/core#linuxcentos
+sudo yum install -y libunwind libicu
+curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?linkid=848821
+sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+
+# This section currently throws an error on amazon linux
+# Install docker CE: https://docs.docker.com/engine/installation/linux/docker-ce/centos/#install-docker-ce
+#sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# #sudo yum-config-manager --enable docker-ce-edge
+# #sudo yum-config-manager --enable docker-ce-testing
+# #sudo yum-config-manager --disable docker-ce-edge
+# sudo yum makecache fast
+# sudo yum install docker-ce
+
+# Install the CodeDeploy agent
+sudo yum install -y ruby wget
+cd /home/ec2-user
+# TODO: How to parameterize this script for different regions??
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+# Print agent status
+sudo service codedeploy-agent status
